c0bab5db74c7a9d0f3002ced0dfef67dc56a3786
tsviet <tsviet@pdx.edu>
2016-07-31 18:36:09 -0700

    Make camera to take a screenshot and add thumbnail of it to add screen.

--- app/src/main/java/com/cs410_hci/holgater/cs410finalproject/AddProduct.java
+++ app/src/main/java/com/cs410_hci/holgater/cs410finalproject/AddProduct.java
@@ -3,11 +3,13 @@ package com.cs410_hci.holgater.cs410finalproject;
 import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -19,7 +21,9 @@ import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
+import java.io.Console;
 import java.io.File;
+import java.io.FileOutputStream;
 
 public class AddProduct extends AppCompatActivity {
     private static final int TAKE_PICTURE = 1;
@@ -30,47 +34,33 @@ public class AddProduct extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_product);
 
-       /* //setup "products" button in toolbar
+        String n = String.valueOf(PackageManager.GET_PERMISSIONS);
+        Log.d("Creation", n);
+        //setup "products" button in toolbar
         RadioButton productsButton = (RadioButton) findViewById(R.id.Camera);
         productsButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                File photo = new File(Environment.getExternalStorageDirectory(),  "Pic.jpg");
+                /*File photo = new File(Environment.getExternalStorageDirectory(),  "Pic.jpg");
+                photo.deleteOnExit();
                 intent.putExtra(MediaStore.EXTRA_OUTPUT,
                         Uri.fromFile(photo));
-                imageUri = Uri.fromFile(photo);
-                startActivityForResult(intent, TAKE_PICTURE);
+                imageUri = Uri.fromFile(photo);*/
+                startActivityForResult(intent, 1888);
             }
-        });*/
+        });
     }
 
     public void createProduct(View view) {
         finish();
     }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode) {
-            case TAKE_PICTURE:
-                if (resultCode == Activity.RESULT_OK) {
-                    Uri selectedImage = imageUri;
-                    getContentResolver().notifyChange(selectedImage, null);
-                    ImageView imageView = (ImageView) findViewById(R.id.ImageView);
-                    ContentResolver cr = getContentResolver();
-                    Bitmap bitmap;
-                    try {
-                        bitmap = android.provider.MediaStore.Images.Media
-                                .getBitmap(cr, selectedImage);
-
-                        imageView.setImageBitmap(bitmap);
-                        Toast.makeText(this, selectedImage.toString(),
-                                Toast.LENGTH_LONG).show();
-                    } catch (Exception e) {
-                        Toast.makeText(this, "Failed to load", Toast.LENGTH_SHORT)
-                                .show();
-                        Log.e("Camera", e.toString());
-                    }
-                }
+        ImageView imageView = (ImageView) findViewById(R.id.ImageView);
+        if (requestCode == 1888 && resultCode == RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
         }
     }
 }
