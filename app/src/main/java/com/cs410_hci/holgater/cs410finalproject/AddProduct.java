de90fd649b2d0f3aca7b7e88f5af44e0fb9ca985
tsviet <tsviet@pdx.edu>
2016-08-02 13:09:28 -0700

    Add Gallery function Add Cloud message dialog

--- app/src/main/java/com/cs410_hci/holgater/cs410finalproject/AddProduct.java
+++ app/src/main/java/com/cs410_hci/holgater/cs410finalproject/AddProduct.java
@@ -1,29 +1,27 @@
 package com.cs410_hci.holgater.cs410finalproject;
 
-import android.app.Activity;
-import android.content.ContentResolver;
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
-import android.provider.Settings;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
-import java.io.Console;
-import java.io.File;
-import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class AddProduct extends AppCompatActivity {
     private static final int TAKE_PICTURE = 1;
@@ -36,17 +34,42 @@ public class AddProduct extends AppCompatActivity {
 
         String n = String.valueOf(PackageManager.GET_PERMISSIONS);
         Log.d("Creation", n);
-        //setup "products" button in toolbar
+
+        //Use camera to get thumbnail
         RadioButton productsButton = (RadioButton) findViewById(R.id.Camera);
         productsButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                /*File photo = new File(Environment.getExternalStorageDirectory(),  "Pic.jpg");
-                photo.deleteOnExit();
-                intent.putExtra(MediaStore.EXTRA_OUTPUT,
-                        Uri.fromFile(photo));
-                imageUri = Uri.fromFile(photo);*/
                 startActivityForResult(intent, 1888);
+            }
+        });
+
+        //Use gallery to create thumbnail
+        RadioButton galeryButton = (RadioButton) findViewById(R.id.Gallery);
+        galeryButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 18);
+            }
+        });
+
+        //Use gallery to create thumbnail
+        RadioButton cloudButton = (RadioButton) findViewById(R.id.Cloud);
+        cloudButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(AddProduct.this);
+                builder.setTitle("Sorry! This button not available now!")
+                        .setMessage("Wait for update before it will be available!")
+                        .setCancelable(true)
+                        .setNegativeButton("ОК",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+                AlertDialog alert = builder.create();
+                alert.show();
             }
         });
     }
@@ -61,6 +84,20 @@ public class AddProduct extends AppCompatActivity {
         if (requestCode == 1888 && resultCode == RESULT_OK) {
             Bitmap photo = (Bitmap) data.getExtras().get("data");
             imageView.setImageBitmap(photo);
+        } else if (requestCode == 18 && resultCode == RESULT_OK) {
+            Uri imageUri = data.getData();
+            Bitmap bitmap;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(
+                        getContentResolver(), imageUri);
+                imageView.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
         }
     }
 }
